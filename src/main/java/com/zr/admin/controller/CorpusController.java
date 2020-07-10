c5fdea6da300a91fd46f43d5a605934202a47442
zhangrui <1570266831@qq.com>
2020-07-10 19:00:03 +0800

    导入包修改

--- src/main/java/com/zr/admin/controller/CorpusController.java
+++ src/main/java/com/zr/admin/controller/CorpusController.java
@@ -9,7 +9,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.multipart.MultipartFile;
 
 import java.util.Iterator;
 import java.util.List;
